72ec23d2395157a80e82c9ed3ab38be274460288
Luciano Lopes <lucianolpsf@gmail.com>
2024-02-25 01:14:38 -0300

    implementando testimonials carrossel animation

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(function() {
+    "use strict";
+  
+    new Swiper('.testimonials-slider-comments', {
+      speed: 600,
+      loop: true,
+      autoplay: {
+        delay: 3500,
+        disableOnInteraction: false
+      },
+      slidesPerView: 'auto',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+        clickable: true
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 20
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 20
+        }
+      }
+    });
+  
+  })()
