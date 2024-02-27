42108719be21528d3281da0198ba2a7dffb229d5
Luciano Lopes <lucianolpsf@gmail.com>
2024-02-27 10:33:50 -0300

    Ajustes de syles em smartphones e alinhamento de carrossel de comentarios

--- js/index.js
+++ js/index.js
@@ -13,18 +13,28 @@
         el: '.swiper-pagination',
         type: 'bullets',
         clickable: true
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 1,
-          spaceBetween: 20
-        },
-  
-        1200: {
-          slidesPerView: 3,
-          spaceBetween: 20
-        }
       }
+      // breakpoints: {
+      //   320: {
+      //     slidesPerView: 1,
+      //     spaceBetween: 20
+      //   },
+  
+      //   1000: {
+      //     slidesPerView: 2,
+      //     spaceBetween: 20
+      //   },
+  
+      //   1200: {
+      //     slidesPerView: 3,
+      //     spaceBetween: 40
+      //   },
+
+      //   1440: {
+      //     slidesPerView: 3,
+      //     spaceBetween: 20
+      //   }
+      // }
     });
   
   })()
